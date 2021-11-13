28fbdfca4fcf5fda4c9f1663c2f2fa3da511c45f
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-11-13 08:12:34 +0000

    add validation form

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,8 +1,24 @@
-import { Form as VeeForm, Field as VeeField } from 'vee-validate';
+import {
+  Form as VeeForm, Field as VeeField, defineRule, ErrorMessage,
+} from 'vee-validate';
+import {
+  required, min, max, alpha_spaces as alphaSpaces, email,
+  min_value as minValue, max_value as maxValue, confirmed,
+} from '@vee-validate/rules';
 
 export default {
   install(app) {
     app.component('VeeForm', VeeForm);
     app.component('VeeField', VeeField);
+    app.component('ErrorMessage', ErrorMessage);
+
+    defineRule('required', required);
+    defineRule('min', min);
+    defineRule('max', max);
+    defineRule('alpha_space', alphaSpaces);
+    defineRule('email', email);
+    defineRule('min_value', minValue);
+    defineRule('max_value', maxValue);
+    defineRule('confirmed', confirmed);
   },
 };
