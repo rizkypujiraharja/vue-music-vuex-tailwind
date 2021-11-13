9e4f445c527f7517e035eae866dd8db972e92912
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-11-13 07:03:42 +0000

    switch tab login register

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -31,17 +31,31 @@
           <!-- Tabs -->
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition hover:text-white text-white
-                bg-blue-600" href="#">Login</a>
+              <a class="block rounded py-3 px-4 transition"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab == 'login',
+                  'hover:text-blue-600': tab == 'register',
+                }"
+                href="#" @click.prevent="tab = 'login'"
+              >
+                Login
+              </a>
             </li>
             <li class="flex-auto text-center">
               <a class="block rounded py-3 px-4 transition"
-                href="#">Register</a>
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab == 'register',
+                  'hover:text-blue-600': tab == 'login',
+                }"
+                href="#" @click.prevent="tab = 'register'"
+              >
+                Register
+              </a>
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-if="tab == 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -65,7 +79,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <form v-if="tab == 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -138,6 +152,11 @@ import { mapGetters, mapMutations } from 'vuex';
 
 export default {
   name: 'Auth',
+  data() {
+    return {
+      tab: 'login',
+    };
+  },
   computed: {
     ...mapGetters(['authModalShow']),
   },
