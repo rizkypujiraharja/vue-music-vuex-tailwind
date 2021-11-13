e829ffa490456125c32640afe22a5242aa3735a9
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-11-13 12:15:56 +0000

    custom trigger validation

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -42,6 +42,10 @@ export default {
 
         return message;
       },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
     });
   },
 };
