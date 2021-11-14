9cfaea63e96c46b68de773c1c47d45729ddf8ffd
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-11-14 16:07:30 +0000

    change logged in user menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,14 +9,19 @@
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
-          <li>
+          <li v-if="!userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">
               Login / Register
             </a>
           </li>
-          <li>
-            <a class="px-2 text-white" href="#">Manage</a>
-          </li>
+          <template v-else>
+            <li>
+              <a class="px-2 text-white" href="#">Manage</a>
+            </li>
+            <li>
+              <a class="px-2 text-white" href="#">Logout</a>
+            </li>
+          </template>
         </ul>
       </div>
     </nav>
@@ -24,10 +29,13 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
+import { mapMutations, mapState } from 'vuex';
 
 export default {
   name: 'Header',
+  computed: {
+    ...mapState(['userLoggedIn']),
+  },
   methods: {
     ...mapMutations([
       'toggleAuthModal',
