80bcc31ba0ba4036a9c9345792efdc78642f5da6
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-11-21 09:12:37 +0000

    Manage page

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Manage from '../views/Manage.vue';
 import Home from '../views/Home.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/manage',
+    name: 'manage',
+    component: Manage,
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-600',
 });
 
 export default router;
